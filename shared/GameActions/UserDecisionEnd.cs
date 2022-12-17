6056dac73a2b92500f9a4e1f7533fb4767dd14a3
k0c0w <107144084+k0c0w@users.noreply.github.com>
2022-12-17 20:00:30 +0300

    shared lib (packets, actions, cards, packetencoder) (#4)

---
+++ shared/GameActions/UserDecisionEnd.cs
@@ -0,0 +1,5 @@
+﻿namespace Shared.GameActions;
+public record UserDecisionEnd : GameAction
+{
+    
+}
---
+++ shared/GameActions/UserPutCard.cs
@@ -0,0 +1,11 @@
+﻿using Shared.PossibleCards;
+
+namespace Shared.GameActions;
+
+public record UserPutCard : CardAction, IBothUserInfo
+{
+    public UserPutCard(int userId, int line, AllCards card, int indexInHand = -1) : base(userId, line, card)
+        => IndexInHand = indexInHand;
+    
+    public int IndexInHand { get;}
+} 
