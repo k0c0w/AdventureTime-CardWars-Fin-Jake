6acbfecc959ebc35ecbcb2fe5db28d4ab52724d6
Artur Yakupov <art.kazan@mail.ru>
2022-12-18 18:29:30 +0300

    Created cards maps

---
+++ CardWarsClient/ClientLogic/Cards/Land.cs
@@ -0,0 +1,19 @@
+﻿using Shared.PossibleCards;
+
+namespace CardWarsClient.ClientLogic.Cards;
+
+public class Land : Card
+{
+    public bool IsTurnedOut { get; private set; }
+    
+    public LandType LandType { get; }
+
+    public Land(LandType land, string frontSuit, string backSuit) : base(frontSuit, backSuit)
+    {
+        if (LandType.Any == land)
+            throw new ArgumentException("Land can not be Any");
+        LandType = land;
+    }
+
+    public void TurnOut() => IsTurnedOut = !IsTurnedOut;
+}
