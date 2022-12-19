0cdeed574ee32fb1392e8542e4738d0056cc8244
Artur Yakupov <art.kazan@mail.ru>
2022-12-19 21:00:58 +0300

    registering user decision start

--- GameLogic/GameKernel/Game/GameStates/UserChooseDeck.cs
+++ GameLogic/GameKernel/Game/GameStates/UserChooseDeck.cs
@@ -44,9 +44,9 @@ public class UserChooseDeck : IGameState
 
     public void ChangeState()
     {
-        CurrentGame.RegisterAction(new GameStart());
+        CurrentGame.RegisterAction(new UserDecisionStart {UserId = 1});
         CurrentGame._holder = null!;
-        CurrentGame.GameState = new TakeCardsState(1, CurrentGame);
+        CurrentGame.GameState = new TakeCardsState(1, CurrentGame, true);
         CurrentGame.GameState.Execute(new GameAction() {UserId = 1});
         CurrentGame.GameState = new AfterGameStartPlayerDecision(CurrentGame);
     }
